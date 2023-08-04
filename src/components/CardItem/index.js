7082f69f7a97bf804ec2acbf041a1881befabee8
= <=>
2023-08-04 08:25:57 +0000

    technology cards

---
+++ src/components/CardItem/index.js
@@ -0,0 +1,16 @@
+import './index.css'
+
+const CardItem = props => {
+  const {cardsList, key} = props
+  const {title, className, description, imageUrl} = cardsList
+  return (
+    <li className="bg-container">
+      <div className={className}>
+        <h1 className="heading">{title}</h1>
+        <p className="paragraph">{description} </p>
+        <img src={imageUrl} alt={title} className="image" />
+      </div>
+    </li>
+  )
+}
+export default CardItem
